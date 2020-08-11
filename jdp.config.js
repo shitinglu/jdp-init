02bc2ed977f792176a006af7be847708a7635240
shitinglu <shitinglu@jd.com>
2020-08-11 15:34:24 +0800

    1

---
+++ jdp.config.js
@@ -0,0 +1,38 @@
+
+// 目录
+const dir = 'dirName';
+
+// 版本号
+const version = 20190904;
+
+let configs = {
+    routeConfig: {
+        js: `${dir}/${version}/js/`,
+        img: `${dir}/${version}/img/`,
+        css: {
+            path: `${dir}/${version}/css`
+        },
+        html: {
+            needInject: false,
+            template: 'src/index.html',
+            outName: 'index.html'
+        }
+    },
+    rem: {
+        need: true, //是否需要REM转换
+        psdWidth: 750
+    },
+    cdn: {
+        online: '',
+        beta: ''
+    },
+    git: {
+        url: '',
+        branch: '',
+        sourseDir:dir,
+        gitDirName:''
+    },
+    historyApi:false
+};
+
+module.exports = configs
---
+++ src/js/pack-index.js
@@ -0,0 +1,4 @@
+// 引入样式
+import '../sass/index.scss';
+
+console.log('欢迎使用jdp 开发');
